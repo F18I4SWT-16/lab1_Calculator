a3588a29267d08d6f01a3900256828534b21e9dd
Lars Lippert Ovesen <au556902@uni.au.dk>
2018-02-01 11:37:07 +0100

    Revert "Revert "Uploaded project""
    This reverts commit 12f9b7408a7c69d8abb235599e0f58cec6ddd8ab.

---
+++ Lab1_Calculator.library/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Calculator.Library
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double result = x;
+
+            if (exp == 0)
+            {
+                result = 1;
+            }
+            for (int i = 1; i < exp; i++)
+            {
+                result = result * x;
+            }
+
+            return result;
+        }
+    }
+}
