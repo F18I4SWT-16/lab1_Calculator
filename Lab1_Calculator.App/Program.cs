4cb5b3edf6a992382f3a779296329ed2301b851e
2takker <2takker@gmail.com>
2018-02-01 11:24:18 +0100

    Uploaded project

---
+++ Lab1_Calculator.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1_Calculator.Library;
+
+namespace Lab1_Calculator.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("2 + 2 = " + calc.Add(2,2));
+
+            Console.WriteLine("2 - 2 = " + calc.Subtract(2,2));
+
+            Console.WriteLine("2 * 2 = " + calc.Multiply(2,2));
+
+            Console.WriteLine("2^2 = " + calc.Power(2,2));
+
+            Console.WriteLine("2^1 = " + calc.Power(2,1));
+
+            Console.WriteLine("2^0 = " + calc.Power(2,0));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1_Calculator.Unit.Test/CalculatorUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1_Calculator.Library;
+using NUnit.Framework;
+
+namespace Lab1_Calculator.Unit.Test
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private Calculator _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new Calculator();
+        }
+
+        [Test]
+        public void Add_AddTwoNumbers_NumbersIsAdded()
+        {
+            Assert.That(_uut.Add(2,2), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Subtract_SubtractTwoNumbers_NumbersIsSubtracted()
+        {
+            Assert.That(_uut.Subtract(2,2), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Multiply_MultiplyTwoNumbers_NumbersIsMultiplied()
+        {
+            Assert.That(_uut.Multiply(2,2), Is.EqualTo(4));
+        }
+
+        [TestCase(2,2,4)]
+        [TestCase(2,1,2)]
+        [TestCase(2,0,1)]
+        public void Power_FindPowerOfNumber_PowerIsCorrect(double x, double exp, double expected)
+        {
+            Assert.That(_uut.Power(x,exp), Is.EqualTo(expected));
+        }
+
+    }
+}
